1f37653268914e573a305d02f515bbf3447f32c8
Andrew Wong <andrew.wong.dgnt@gmail.com>
2018-01-30 00:38:16 -0800

    More UI upgrades

--- CocoBlitz/Assets/Game/GameUtil.cs
+++ CocoBlitz/Assets/Game/GameUtil.cs
@@ -6,9 +6,9 @@ public class GameUtil  {
 
     public enum GameModeEnum { FastestTime, RackUpThePoints };
 
-    public static GameModeEnum currentGameMode = GameModeEnum.FastestTime;
-    public static float timer = 10;
-    public static int pointsToReach = 10;
+    public static GameModeEnum currentGameMode = GameModeEnum.RackUpThePoints;
+    public static float timer = 5;
+    public static int pointsToReach = 5;
 
     public static List<Cpu> cpuList = new List<Cpu>();
 }
